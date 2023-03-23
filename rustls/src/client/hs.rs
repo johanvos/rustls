8f7e9d171114daff7f002dda92121698f9a43978
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-03-23 11:28:27 +0100

    client: check enabled_tickets up front

--- rustls/src/client/hs.rs
+++ rustls/src/client/hs.rs
@@ -384,6 +384,10 @@ fn prepare_resumption<'a>(
     cx: &mut ClientContext<'_>,
     config: &ClientConfig,
 ) -> Option<persist::Retrieved<&'a persist::Tls13ClientSessionValue>> {
+    if !config.enable_tickets {
+        return None;
+    }
+
     let (ticket, resume_version) = if let Some(resuming) = resuming {
         match &resuming.value {
             ClientSessionValue::Tls13(inner) => (inner.ticket().to_vec(), ProtocolVersion::TLSv1_3),
@@ -395,7 +399,6 @@ fn prepare_resumption<'a>(
     };
 
     if config.supports_version(ProtocolVersion::TLSv1_3)
-        && config.enable_tickets
         && resume_version == ProtocolVersion::TLSv1_3
         && !ticket.is_empty()
     {
@@ -415,7 +418,7 @@ fn prepare_resumption<'a>(
                 tls13::prepare_resumption(&config, cx, &resuming, exts, doing_retry);
                 resuming
             })
-    } else if config.enable_tickets {
+    } else {
         // If we have a ticket, include it.  Otherwise, request one.
         if ticket.is_empty() {
             exts.push(ClientExtension::SessionTicket(ClientSessionTicket::Request));
@@ -424,8 +427,6 @@ fn prepare_resumption<'a>(
                 Payload::new(ticket),
             )));
         }
-        None
-    } else {
         None
     }
 }
