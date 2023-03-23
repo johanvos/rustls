6af03df20c9f328ab0da7fc19a637dd23bdcf19d
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-03-23 11:51:13 +0100

    client: extract check for TLS 1.2 cipher suite

--- rustls/src/suites.rs
+++ rustls/src/suites.rs
@@ -91,6 +91,7 @@ impl SupportedCipherSuite {
         }
     }
 
+    #[cfg(any(test, feature = "quic"))]
     pub(crate) fn tls13(&self) -> Option<&'static Tls13CipherSuite> {
         match self {
             #[cfg(feature = "tls12")]
